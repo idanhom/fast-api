121e54ef217bf66ed53eb7724bec28c81907cdfe
Oscar Pettersson <pson93@gmail.com>
2025-06-21 11:08:06 +0200

    init

---
+++ s2. python installer and refresher/16.py
@@ -0,0 +1,8 @@
+"""Write a Python program that can do the following:
+
+- You have $50
+
+- You buy an item that is $15, that has a 3% tax
+
+- Using the print()  Print how much money you have left, after purchasing the item."""
+
