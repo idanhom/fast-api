45996b5c083f768b53b4618d94a2627e700db413
Oscar Pettersson <pson93@gmail.com>
2025-07-05 15:30:22 +0200

    push

--- s2. python installer and refresher/37.py
+++
@@ -1,9 +0,0 @@
-def func37(firstname, lastname, age):
-    return {
-        'firstname': firstname,
-        'lastname': lastname,
-        'age': age
-    }
-    
-print(func37('oscar', 'pettersson', 30))
-
