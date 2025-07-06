5fda99b6c24054f421e46be51810896030ef85ed
Oscar Pettersson <pson93@gmail.com>
2025-07-06 07:19:11 +0200

    push

--- fastapi/project_1/books.py
+++ fastapi/project_1/books.py
@@ -4,13 +4,17 @@ app = FastAPI()
 
 BOOKS = [
     {"title": "Title One", "author": "Author One", "category": "Science"},
-    {"title": "Title Two", "author": "Author Two", "category": "History"},
-    {"title": "Title Three", "author": "Author Three", "category": "Math"},
-    {"title": "Title Four", "author": "Author Four", "category": "Science"},
-    {"title": "Title Five", "author": "Author Five", "category": "History"},
+    {"title": "Title Two", "author": "Author Two", "category": "Science"},
+    {"title": "Title Three", "author": "Author Three", "category": "History"},
+    {"title": "Title Four", "author": "Author Four", "category": "Math"},
+    {"title": "Title Five", "author": "Author Five", "category": "Math"},
+    {"title": "Title Six", "author": "Author Two", "category": "Math"}
 ]
 
-
-@app.get("/books")
-async def read_all_books():
-    return BOOKS
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+        
+    
