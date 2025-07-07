64c87f6668c2292c038fa91393d5e56a90e49daf
Oscar Pettersson <pson93@gmail.com>
2025-07-07 06:50:32 +0200

    push

--- fastapi/project_1/books.py
+++ fastapi/project_1/books.py
@@ -8,13 +8,12 @@ BOOKS = [
     {"title": "Title Three", "author": "Author Three", "category": "History"},
     {"title": "Title Four", "author": "Author Four", "category": "Math"},
     {"title": "Title Five", "author": "Author Five", "category": "Math"},
-    {"title": "Title Six", "author": "Author Two", "category": "Math"}
+    {"title": "Title Six", "author": "Author Two", "category": "Math"},
 ]
+
 
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
+        if book.get("title").casefold() == book_title.casefold():
             return book
-        
-    
