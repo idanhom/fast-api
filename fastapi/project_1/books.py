b6301dcdb2091e9f0da3b3c72a5ed8c66df1ccc0
Oscar Pettersson <pson93@gmail.com>
2025-07-10 12:42:35 +0200

    push

--- fastapi/project_1/books.py
+++ fastapi/project_1/books.py
@@ -15,3 +15,12 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.get("books/mybook")
+async def read_all_books():
+    return {'book_title': 'My favorite book!'}
+
+
+@app.get("/books/{dynamic_param}")
+async def read_all_books(dynamic_param: str):
+    return {'dynamic_param': dynamic_param}
