d3ff407d8d912b8c2197ba3d07ac8aa9dd1fcdd1
Oscar Pettersson <pson93@gmail.com>
2025-07-13 22:46:49 +0200

    push

--- fastapi/project_1/books.py
+++ fastapi/project_1/books.py
@@ -21,9 +21,28 @@ BOOKS = [
 
 
 
-@app.get("/books/{book_title}")
-async def read_title(book_title: str):
-    for book in BOOKS:
-        if book['title'].casefold() == book_title.casefold():
-            return book
+# @app.get("/books/{book_title}")
+# async def read_title(book_title: str):
+#     for book in BOOKS:
+#         if book['title'].casefold() == book_title.casefold():
+#             return book
         
+
+@app.get("/books/")
+async def read_books_by_category(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book["category"].casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get("/books/{book_author}")
+async def read_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (
+            book["author"].casefold() == book_author.casefold()
+            and book["category"].casefold == category.casefold()
+        ):
+            books_to_return.append(book)
+    return books_to_return
