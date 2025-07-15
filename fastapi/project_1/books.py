9b9a17a662161cfc12d6f68a1ee6276420abc95c
Oscar Pettersson <pson93@gmail.com>
2025-07-15 06:16:30 +0200

    push

--- fastapi/project_1/books.py
+++ fastapi/project_1/books.py
@@ -11,38 +11,7 @@ BOOKS = [
     {"title": "Title Six", "author": "Author Two", "category": "Math"},
 ]
 
-# @app.get("/books")
-# async def read_all_books():
-#     return BOOKS
 
-# @app.get("/books/mybook")
-# async def read_all_books():
-#     return {'book_title': 'My favorite book!'}
-
-
-
-# @app.get("/books/{book_title}")
-# async def read_title(book_title: str):
-#     for book in BOOKS:
-#         if book['title'].casefold() == book_title.casefold():
-#             return book
-        
-
-@app.get("/books/")
-async def read_books_by_category(category: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book["category"].casefold() == category.casefold():
-            books_to_return.append(book)
-    return books_to_return
-
-@app.get("/books/{book_author}")
-async def read_category_by_query(book_author: str, category: str):
-    books_to_return = []
-    for book in BOOKS:
-        if (
-            book["author"].casefold() == book_author.casefold()
-            and book["category"].casefold == category.casefold()
-        ):
-            books_to_return.append(book)
-    return books_to_return
+@app.get("/books/{book_title}")
+async def read_book(category: str):
+    return [book for book in BOOKS if book["category"].casefold() == category.casefold()]
