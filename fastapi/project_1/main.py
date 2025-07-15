b44e1b3188f50b665e9ea875dca40aff86ca0d03
Oscar Pettersson <pson93@gmail.com>
2025-07-15 05:51:48 +0200

    push

---
+++ fastapi/project_1/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+
+
+
+BOOKS = [
+    {"title": "Title One", "author": "Author One", "category": "Science"},
+    {"title": "Title Two", "author": "Author Two", "category": "Math"},
+    {"title": "Title Three", "author": "Author Three", "category": "History"},
+]
+
