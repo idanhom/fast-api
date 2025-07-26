0aaa8278a5cce7c8b2ee677972724abc17f0041a
Oscar Pettersson <pson93@gmail.com>
2025-07-26 12:29:48 +0200

    push

--- fastapi/project_2/1.py
+++ fastapi/project_2/1.py
@@ -22,7 +22,7 @@ class BookRequest(BaseModel):
                 "author": "A new author",
                 "description": "A new description",
                 "rating": 5,
-                "published_date": "2015"
+                "published_date": 2015
             }
         }
     }
@@ -43,10 +43,18 @@ class Book(BaseModel):
 
 # Fake stored book list (pretend these are from previous POSTs)
 books = [
-    Book(id=0, title="Computer Science Pro", author="Coding with Ruby",
-         description="A very nice book", rating=5, published_date=2016),
-    Book(id=1, title="Be Fast with FastAPI", author="Coding with RGB",
-         description="A great book", rating=5, published_date=2011),
+    Book(id=0, 
+         title="Computer Science Pro", 
+         author="Coding with Ruby",
+         description="A very nice book", 
+         rating=5, 
+         published_date=2016),
+    Book(id=1, 
+         title="Be Fast with FastAPI", 
+         author="Coding with RGB",
+         description="A great book",
+         rating=5, 
+         published_date=2011),
 ]
 
 def assign_id(book_data: dict) -> dict:
@@ -66,12 +74,6 @@ def assign_id(book_data: dict) -> dict:
 async def read_all_books():
     return books
 
-@app.get("/books/{book_id}")
-async def read_book(book_id: int):
-    for book in books:
-        if book.id == book_id:
-            return book
-        
 
 @app.get("/book/")
 async def read_book_by_rating(book_rating: int):
@@ -80,12 +82,31 @@ async def read_book_by_rating(book_rating: int):
         book.rating == book_rating
     ]
 
-@app.get("/books/filter_by/{date}")
+@app.get("/books/publish")
 async def get_books_filter_by_date(date: int):
     return [
         book for book in books if
         book.published_date == date
     ]
+
+@app.post("/create-book", response_model=Book, status_code=201)
+async def create_book(book_request: BookRequest):
+    book_data = book_request.model_dump(exclude_none=True)
+
+    book_data = assign_id(book_data)
+
+    new_book = Book(**book_data)
+    books.append(new_book)
+    return new_book
+
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in books:
+        if book.id == book_id:
+            return book
+        
 
 
 @app.put("/books/{book_id}", response_model=Book)
@@ -99,15 +120,6 @@ async def update_book(book_id: int, payload: BookRequest):
 
 
 
-@app.post("/create-book", response_model=Book, status_code=201)
-async def create_book(book_request: BookRequest):
-    book_data = book_request.model_dump(exclude_none=True)
-
-    book_data = assign_id(book_data)
-
-    new_book = Book(**book_data)
-    books.append(new_book)
-    return new_book
 
 @app.delete("/books/{book_id}")
 async def remove_book(book_id):
