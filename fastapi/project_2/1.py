320c09f8a720211fa41ae33795e3f6cc7d4163ae
Oscar Pettersson <pson93@gmail.com>
2025-07-27 07:38:38 +0200

    added type validation

--- fastapi/project_2/1.py
+++ fastapi/project_2/1.py
@@ -1,6 +1,6 @@
 # https://redeploy.udemy.com/course/fastapi-the-complete-course/learn/lecture/29025634#overview
 
-
+from typing import Optional
 from fastapi import FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel, Field
 
@@ -13,7 +13,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(ge=1, le=5)
-    published_date: int
+    published_date: int = Field(ge=1900, le=2100)
 
     model_config = {
         "json_schema_extra": {
@@ -32,24 +32,24 @@ class BookRequest(BaseModel):
 # Define full book structure (includes ID)
 class Book(BaseModel):
     id: int
-    title: str
-    author: str
-    description: str
-    rating: int
-    published_date: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(ge=1, le=5)
+    published_date: int = Field(ge=1900, le=2100)
 
 
 
 
 # Fake stored book list (pretend these are from previous POSTs)
 books = [
-    Book(id=0, 
+    Book(id=1, 
          title="Computer Science Pro", 
          author="Coding with Ruby",
          description="A very nice book", 
          rating=5, 
          published_date=2016),
-    Book(id=1, 
+    Book(id=2, 
          title="Be Fast with FastAPI", 
          author="Coding with RGB",
          description="A great book",
@@ -58,52 +58,70 @@ books = [
 ]
 
 def assign_id(book_data: dict) -> dict:
-    # if books:
-    #     book_data["id"] = books[-1].id + 1
-    # else:
-    #     book_data["id"] = 1
-    # return book_data
-
-    book_data["id"] = books[-1].id + 1 if books else 1
+    if books:
+        book_data["id"] = books[-1].id + 1
+    else:
+        book_data["id"] = 1
     return book_data
 
 
 
 
 @app.get("/books")
-async def read_all_books() -> list[Book]:
-    return books
+async def read_all_books(rating: Optional[int] = Query(default=None, ge=1, le=5),
+                         published_date: Optional[int] = Query(default=None, ge=1900, le=2100)):
+    filtered_books = books
+    if rating is not None:
+        filtered_books = [book for book in filtered_books if book.rating == rating]
+    if published_date is not None:
+        filtered_books = [book for book in filtered_books if book.published_date == published_date]
+    if not filtered_books:
+        raise HTTPException(status_code=404, detail="Books not found")
+    return filtered_books
 
 
-@app.get("/book/")
-async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
-    matching_books= [
-        book for book in books if
-        book.rating == book_rating
-    ]
+# @app.get("/books/publish")
+# async def get_books_filter_by_date(date: int = Query(gt=1999, lt=2031)):
+#     matching_books = [
+#         book for book in books if
+#         book.published_date == date
+#     ]
 
-    if not matching_books:
-        raise HTTPException(status_code=404, detail="books not found")
-
-    return matching_books
-
-
-@app.get("/books/publish")
-async def get_books_filter_by_date(date: int = Query(gt=1999, lt=2031)):
-    matching_books = [
-        book for book in books if
-        book.published_date == date
-    ]
-
-    if not matching_books:
-        raise HTTPException(status_code=404, detail="books not found")
+#     if not matching_books:
+#         raise HTTPException(status_code=404, detail="books not found")
     
-    return matching_books
+#     return matching_books
+
+
+
+# @app.get("/book/")
+# async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+#     matching_books= [
+#         book for book in books if
+#         book.rating == book_rating
+#     ]
+
+#     if not matching_books:
+#         raise HTTPException(status_code=404, detail="books not found")
+
+#     return matching_books
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int) -> Book:
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    raise HTTPException(status_code=404,  detail="Book not found")
+
+
+
+
 
 
 @app.post("/create-book", response_model=Book, status_code=201)
 async def create_book(book_request: BookRequest):
-    book_data = book_request.model_dump(exclude_none=True)
+    book_data = book_request.model_dump()
 
     book_data = assign_id(book_data)
 
@@ -113,13 +131,6 @@ async def create_book(book_request: BookRequest):
 
 
 
-@app.get("/books/{book_id}")
-async def read_book(book_id: int) -> Book:
-    for book in books:
-        if book.id == book_id:
-            return book
-
-    raise HTTPException(status_code=404, detail="books not found")
 
 
 @app.put("/books/{book_id}", response_model=Book)
@@ -138,5 +149,5 @@ async def remove_book(book_id: int = Path(gt=0)) -> None:
         if content.id == book_id:
             books.pop(idx)
             return
-    raise HTTPException(status_code=404, detail="book not found")
+    raise HTTPException(status_code=404, detail="Book not found")
 
